8d2302e804bda70c7b72e016840337d581c83b6e
iamseeley <seeleypersonal@gmail.com>
2024-07-15 10:47:40 -0400

    added glow to links

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
   setActiveLink();
   createFlowerField();
+  applyGlow()
 });
 
 const setActiveLink = () => {
@@ -61,4 +62,26 @@ const createFlowerField = () => {
     const y = Math.floor(Math.random() * (40 - 5));
     createFlower(x, y);
   }
+};
+
+const applyGlow = () => {
+  const links = document.querySelectorAll('a:not(nav a)');
+
+  links.forEach(link => {
+    const glowPoint = document.createElement('div');
+    glowPoint.classList.add('glow-point');
+    link.appendChild(glowPoint);
+
+    link.addEventListener('mouseenter', () => {
+      glowPoint.style.opacity = '1';
+      glowPoint.style.transform = 'translate(-50%, -50%) scale(1)';
+      glowPoint.style.filter = 'blur(15px)';
+    });
+
+    link.addEventListener('mouseleave', () => {
+      glowPoint.style.opacity = '0';
+      glowPoint.style.transform = 'translate(-50%, -50%) scale(0.1)';
+      glowPoint.style.filter = 'blur(20px)';
+    });
+  });
 };
